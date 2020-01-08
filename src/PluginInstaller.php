2565e22db63f3b1139aee9ac51ce47db8246f092
qq374098596 <374098596@qq.com>
2020-01-08 17:18:02 +0800

    wang

--- src/PluginInstaller.php
+++ src/PluginInstaller.php
@@ -20,7 +20,7 @@ class PluginInstaller extends LibraryInstaller
             );
         }
        // 返回指定路径
-        return './data/'.substr($package->getPrettyName(), 23);
+        return './data/wang/'.substr($package->getPrettyName(), 23);
     }
     /**
      * {@inheritDoc}
