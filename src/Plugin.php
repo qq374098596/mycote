d1fafe9ee56434d3ebb70922e8153c82ad07ecee
qq374098596 <374098596@qq.com>
2020-01-08 14:47:25 +0800

    test composer

---
+++ src/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace qq374098596\mycote;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        
+        $installer = new PluginInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
+?>
---
+++ src/PluginInstaller.php
@@ -0,0 +1,34 @@
+<?php
+namespace qq374098596\mycote;
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+class PluginInstaller extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        // $package->getPrettyName() 包的名字
+        $prefix = substr($package->getPrettyName(), 0, 23);
+        
+        if ('topthink/think' !== $prefix) {
+            throw new \InvalidArgumentException(
+                'Unable to install template, phpdocumentor templates '
+                .'should always start their package name with '
+                .'"phpdocumentor/template-"'
+            );
+        }
+       // 返回指定路径
+        return './data/templates/'.substr($package->getPrettyName(), 23);
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+       
+        return 'project' === $packageType;
+    }
+}
+?>
