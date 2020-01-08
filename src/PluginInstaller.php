c76c10cd7a5b486786f8707b725ef1d7a2403666
qq374098596 <374098596@qq.com>
2020-01-08 16:12:51 +0800

    composer

--- src/PluginInstaller.php
+++ src/PluginInstaller.php
@@ -20,7 +20,7 @@ class PluginInstaller extends LibraryInstaller
             );
         }
        // 返回指定路径
-        return './data/templates/'.substr($package->getPrettyName(), 23);
+        return './'.substr($package->getPrettyName(), 23);
     }
     /**
      * {@inheritDoc}
