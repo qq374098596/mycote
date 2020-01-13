6166b727efb4e917fb77fc2ec35a151113528850
qq374098596 <374098596@qq.com>
2020-01-13 09:18:12 +0800

    wang

--- src/PluginFramework.php
+++ src/PluginFramework.php
@@ -8,17 +8,17 @@ use Composer\Repository\InstalledRepositoryInterface;
 
 class PluginFramework extends Filesystem
 {
-    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        parent::install($repo, $package);
-        $this->filesystem->copyThenRemove('./thinkphp/', './think/thinkphp/');
-    }
+    // public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+    // {
+    //     parent::install($repo, $package);
+    //     $this->filesystem->copyThenRemove('./thinkphp/', './think/thinkphp/');
+    // }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($packageType)
-    {
-        return 'think-framework' === $packageType;
-    }
+    // /**
+    //  * {@inheritDoc}
+    //  */
+    // public function supports($packageType)
+    // {
+    //     return 'think-framework' === $packageType;
+    // }
 }
